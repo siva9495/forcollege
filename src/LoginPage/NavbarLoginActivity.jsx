91dbc407f4de5a7320e69f9613bc97f004a519ca
siva9495 <sivaprasad.ksp01@gmail.com>
2024-11-14 16:49:02 +0530

    added the nav and login activity

---
+++ src/LoginPage/NavbarLoginActivity.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarLoginActivity = () => {
+  return (
+    <div className='navbarlogin_container'>
+        
+    </div>
+  )
+}
+
+export default NavbarLoginActivity;
---
+++ src/LoginPage/UserLoginActivity.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserLoginActivity = () => {
+  return (
+    <div>UserLoginActivity</div>
+  )
+}
+
+export default UserLoginActivity;
