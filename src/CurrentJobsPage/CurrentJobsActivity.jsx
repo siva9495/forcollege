7e6561526045bf8a832f742df8ec289bf2243ce9
siva9495 <sivaprasad.ksp01@gmail.com>
2024-11-22 15:41:35 +0530

    html of the table current jobs is done

--- src/CurrentJobsPage/CurrentJobsActivity.jsx
+++ src/CurrentJobsPage/CurrentJobsActivity.jsx
@@ -12,13 +12,36 @@ const CurrentJobsActivity = () => {
                 <table>
                     <thead>
                         <tr className='cj_table_head'>
-                            <th></th>
+                            <th className='table_cj_s_no'><h4>S.No</h4></th>
+                            <th className='table_cj_campus'><h4>Campus</h4></th>
+                            <th className='table_cj_nof_positions'>Name of the Position<h4></h4></th>
+                            <th className='table_cj_no_f_position'><h4>No. of Position</h4></th>
+                            <th className='table_cj_exp_req'><h4>Experience Required</h4></th>
+                            <th className='table_cj_job_ref_no'><h4>Job Reference No.</h4></th>
+                            <th className='table_cj_action'><h4>Action</h4></th>
                         </tr>
                     </thead>
+                    <tbody>
+                        <tr className='table_cj_tr01'>
+                            <td className='table_cj_td_s_no'>1</td>
+                            <td className='table_cj_td_campus'>Hyderabad</td>
+                            <td className='table_cj_td_nof_position'>Assistant Professor</td>
+                            <td className='table_cj_td_no_f_position'>10</td>
+                            <td className='table_cj_td_exp_req'>0 to 15 years</td>
+                            <td className='table_cj_td_job_ref_no'>CBIT ECE HYD-AP/00001</td>
+                            <td className='table_cj_td_action'>
+                                <div className='div_table_cj_btn_v_details'>
+                                    <div className='action_cj_btn_container'>
+                                        <span id='action_cj_btn_span01'>View Details</span>
+                                    </div>    
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
             <div className='cj_table_show_entiies'>
-                
+
             </div>
         </div>
     </div>
