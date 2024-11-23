755a91532738443eb278597be8de4a2863cd8cd0
siva9495 <sivaprasad.ksp01@gmail.com>
2024-11-23 15:21:47 +0530

    changes made on the personaldetails activity for input

--- src/PersonalDetailsPage/PersonalDetailsActivity.jsx
+++ src/PersonalDetailsPage/PersonalDetailsActivity.jsx
@@ -12,57 +12,57 @@ const PersonalDetailsActivity = () => {
                 <div className='pd_userenter_fr01'>
                     <div className='ue_fr01_inner01'>
                         <span className='ue_span_fr01_inner01'>Date of Birth</span>
-                        <input className='ue_input_fr01_inner01' type="text" name="name" placeholder="Enter Name" />
+                        <input className='ue_input_fr01_inner01' type="text" name="dateofbirth" placeholder="Enter DOB" />
                     </div>
                     <div className='ue_fr01_inner02'>
-                        <span className='ue_span_fr01_inner02'>Date of Birth</span>
-                        <input className='ue_input_fr01_inner02' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr01_inner02'>Gender</span>
+                        <input className='ue_input_fr01_inner02' type="text" name="gender" placeholder="Select Gender" />
                     </div>
                     <div className='ue_fr01_inner03'>
-                        <span className='ue_span_fr01_inner03'>Date of Birth</span>
-                        <input className='ue_input_fr01_inner03' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr01_inner03'>Nationality</span>
+                        <input className='ue_input_fr01_inner03' type="text" name="nationality" placeholder="" />
                     </div>
                 </div>
                 <div className='pd_userenter_fr02'>
                     <div className='ue_fr02_inner01'>
-                        <span className='ue_span_fr02_inner01'>Date of Birth</span>
-                        <input className='ue_input_fr02_inner01' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr02_inner01'>Martial Status</span>
+                        <input className='ue_input_fr02_inner01' type="text" name="martial status" placeholder="Enter Name" />
                     </div>
                     <div className='ue_fr02_inner02'>
-                        <span className='ue_span_fr02_inner02'>Date of Birth</span>
-                        <input className='ue_input_fr02_inner02' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr02_inner02'>Personal Mobile Number ISD Code</span>
+                        <input className='ue_input_fr02_inner02' type="text" name="personal mobile number isd code" placeholder="Enter Name" />
                     </div>
                     <div className='ue_fr02_inner03'>
-                        <span className='ue_span_fr02_inner03'>Date of Birth</span>
-                        <input className='ue_input_fr02_inner03' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr02_inner03'>Personal Mobile Number</span>
+                        <input className='ue_input_fr02_inner03' type="text" name="personal mobile number" placeholder="Enter Personal Mobile Number" />
                     </div>
                 </div>
                 <div className='pd_userenter_fr03'>
                     <div className='ue_fr03_inner01'>
-                        <span className='ue_span_fr03_inner01'>Date of Birth</span>
-                        <input className='ue_input_fr03_inner01' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr03_inner01'>Alternative Email ID</span>
+                        <input className='ue_input_fr03_inner01' type="text" name="alternative email id" placeholder="Enter Alternative Email ID" />
                     </div>
                     <div className='ue_fr03_inner02'>
-                        <span className='ue_span_fr03_inner02'>Date of Birth</span>
-                        <input className='ue_input_fr03_inner02' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr03_inner02'>Alternative Mobile Number ISD Code</span>
+                        <input className='ue_input_fr03_inner02' type="text" name="alternative mobile number isd code" placeholder="Enter Alternative Mobile Number ISD Code" />
                     </div>
                     <div className='ue_fr03_inner03'>
-                        <span className='ue_span_fr03_inner03'>Date of Birth</span>
-                        <input className='ue_input_fr03_inner03' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr03_inner03'>Alternative Mobile Number</span>
+                        <input className='ue_input_fr03_inner03' type="text" name="alternative mobile number" placeholder="Enter Alternative Mobile Number" />
                     </div>
                 </div>
                 <div className='pd_userenter_fr04'>
                 <div className='ue_fr04_inner01'>
-                        <span className='ue_span_fr04_inner01'>Date of Birth</span>
-                        <input className='ue_input_fr04_inner01' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr04_inner01'>Physically Challenged</span>
+                        <input className='ue_input_fr04_inner01' type="text" name="physically challenged" placeholder="Enter Name" />
                     </div>
                     <div className='ue_fr04_inner02'>
-                        <span className='ue_span_fr04_inner02'>Date of Birth</span>
-                        <input className='ue_input_fr04_inner02' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr04_inner02'>Pincode</span>
+                        <input className='ue_input_fr04_inner02' type="text" name="pincode" placeholder="Enter Pincode" />
                     </div>
                     <div className='ue_fr04_inner03'>
-                        <span className='ue_span_fr04_inner03'>Date of Birth</span>
-                        <input className='ue_input_fr04_inner03' type="text" name="name" placeholder="Enter Name" />
+                        <span className='ue_span_fr04_inner03'>Address</span>
+                        <input className='ue_input_fr04_inner03' type="text" name="address" placeholder="Enter Address" />
                     </div>
                 </div>
             </div>
