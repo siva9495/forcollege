a9d189bf0bb9e7c3eb613d6ad01697c16fa83bbc
siva9495 <sivaprasad.ksp01@gmail.com>
2025-02-03 19:42:35 +0530

    these changes on the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import DashboardActivity from './DashboardPage/DashboardActivity';
+import HomePageActivity from './LoginPage/HomePageActivity';
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <Router>
         <Routes>
           {/* Redirect root path "/" to "/dashboard" */}
-          <Route path="/" element={<Navigate to="/" replace />} />
-          <Route path="/*" element={<DashboardActivity />} />
+          {/* <Route path="/" element={<Navigate to="/" replace />} />
+          <Route path="/*" element={<DashboardActivity />} /> */}
+          <Route path='/' element={<HomePageActivity/>} />
         </Routes>
       </Router>
     </>
